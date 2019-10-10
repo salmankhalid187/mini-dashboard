9747722c7a8e4674677d12d2afdfd2e9adf3336b
salmankhalid187 <salmankhalid1@gmail.com>
2019-10-10 11:49:28 +0500

    Removed unused import

--- src/app/landing/landing-page/landing-page.component.ts
+++ src/app/landing/landing-page/landing-page.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { PageAppearence, PageProperties } from '../../page-properties';
-import { link } from 'fs';
 
 interface DemoItem {
   title: string;
